fb7cfd5eac8367df722378e41cd513f176a6fc54
prakasham-om <prakashchandrasahoo8991@gmail.com>
2024-02-18 12:11:56 +0530

    hello

--- src/components/SimpleCalculator.jsx
+++ src/components/SimpleCalculator.jsx
@@ -90,6 +90,7 @@ const SimpleCalculator = () => {
                 value={highValue}
                 onChange={(e) => handleInputChange(e, setHighValue)}
                 className='px-1 py-2 border rounded-md text-black w-32'
+                placeholder='Input A'
               />
 
               <input
@@ -97,6 +98,7 @@ const SimpleCalculator = () => {
                 value={lowValue}
                 onChange={(e) => handleInputChange(e, setLowValue)}
                 className='px-1 py-2 border rounded-md text-black w-32'
+                placeholder='Input B'
               />
           
   
@@ -105,6 +107,7 @@ const SimpleCalculator = () => {
                 value={inputB}
                 onChange={(e) => handleInputChange(e, setInputB)}
                 className='px-1 py-2 border rounded-md text-black w-32'
+                placeholder='Input C'
               />
         
           </div>
