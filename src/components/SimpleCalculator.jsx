b48bdc6ef12299dc91feb18efe60ab544ca94515
prakasham-om <prakashchandrasahoo8991@gmail.com>
2024-02-18 11:22:15 +0530

    hello

--- src/components/SimpleCalculator.jsx
+++ src/components/SimpleCalculator.jsx
@@ -4,83 +4,65 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 const SimpleCalculator = () => {
   const [inputA, setInputA] = useState('');
   const [inputB, setInputB] = useState('');
+  const [highValue, setHighValue] = useState('');
+  const [lowValue, setLowValue] = useState('');
   const [result, setResult] = useState(null);
   const [calculationMode, setCalculationMode] = useState('percentage'); // Default mode is percentage
   const [computedValues, setComputedValues] = useState([]);
   const [showTable, setShowTable] = useState(false);
   const [positiveValues, setPositiveValues] = useState([]);
   const [negativeValues, setNegativeValues] = useState([]);
-  const [midpointValues, setMidpointValues] = useState([]);
-  const [point,setPoint]=useState(0);
+  const [point, setPoint] = useState(0);
 
   const handleInputChange = (e, setInput) => {
     setInput(e.target.value);
   };
 
   const calculateResult = () => {
-    const A = parseFloat(inputA);
     const B = parseFloat(inputB);
 
-    if (isNaN(A) || isNaN(B)) {
+    if (isNaN(B)) {
       setResult('Invalid input');
       return;
     }
 
     let calculatedResult;
     if (calculationMode === 'percentage') {
-      calculatedResult = (A * B) / 100 + B;
+      calculatedResult = (parseFloat(inputA) * B) / 100 + B;
     } else {
-      calculatedResult = ((A - B) / B) * 100;
+      calculatedResult = ((parseFloat(inputA) - B) / B) * 100;
     }
     setResult(calculatedResult.toFixed(3));
 
-    localStorage.setItem('calculatorInputA', inputA);
     localStorage.setItem('calculatorInputB', inputB);
     localStorage.setItem('calculatorResult', calculatedResult.toFixed(3));
 
     // Calculate values by adding and subtracting 0.5, 1, 2, and 3 from the result
-    const values = [0.5, 1,1.5,2,2.5, 3].map(val => parseFloat(val.toFixed(1)));
+    const values = [0.5, 1, 1.5, 2, 2.5, 3,3.5,4,4.5,5].map(val => parseFloat(val.toFixed(1)));
     setComputedValues(values);
     
     // Calculate positive and negative values
-    const positive = values.map(val => (A + (val/ 100) * A).toFixed(2));
-    const negative = values.map(val => (A - (val / 100) * A).toFixed(2));
+    const positive = values.map(val => (parseFloat(inputA) + (val / 100) * parseFloat(inputA)).toFixed(2));
+    const negative = values.map(val => (parseFloat(inputA) - (val / 100) * parseFloat(inputA)).toFixed(2));
     setPositiveValues(positive);
     setNegativeValues(negative);
-    const diff=positive[1]-positive[0];
-    setPoint((diff/4).toFixed(2));
-
-    // Calculate midpoint values
-    const midpoints = [];
-    for (let i = 0; i < positive.length - 1; i++) {
-      const positiveMidpoint = (parseFloat(positive[i]) + parseFloat(positive[i + 1])) / 2;
-      const negativeMidpoint = (parseFloat(negative[i]) + parseFloat(negative[i + 1])) / 2;
-      midpoints.push({ positive: positiveMidpoint.toFixed(2), negative: negativeMidpoint.toFixed(2) });
-    }
-    setMidpointValues(midpoints);
+    const diff = positive[1] - positive[0];
+    setPoint((diff / 4).toFixed(2));
   };
 
   const clearStorage = () => {
-    localStorage.removeItem('calculatorInputA');
     localStorage.removeItem('calculatorInputB');
     localStorage.removeItem('calculatorResult');
-    setInputA('');
     setInputB('');
     setResult(null);
     setComputedValues([]);
     setPositiveValues([]);
     setNegativeValues([]);
-    setMidpointValues([]);
   };
 
   useEffect(() => {
-    const storedInputA = localStorage.getItem('calculatorInputA');
     const storedInputB = localStorage.getItem('calculatorInputB');
     const storedResult = localStorage.getItem('calculatorResult');
-
-    if (storedInputA) {
-      setInputA(storedInputA);
-    }
 
     if (storedInputB) {
       setInputB(storedInputB);
@@ -89,7 +71,13 @@ const SimpleCalculator = () => {
     if (storedResult) {
       setResult(storedResult);
     }
-  }, []);
+
+    // Calculate Input A based on high and low values
+    if (highValue && lowValue) {
+      const midpoint = (parseFloat(highValue) + parseFloat(lowValue)) / 2;
+      setInputA(midpoint > parseFloat(inputB) ? highValue : lowValue);
+    }
+  }, [inputB, highValue, lowValue]);
 
   return (
     <div className='w-full'>
@@ -101,7 +89,7 @@ const SimpleCalculator = () => {
               <input
                 type='number'
                 value={inputA}
-                onChange={(e) => handleInputChange(e, setInputA)}
+                readOnly
                 className='px-4 py-2 border rounded-md text-black'
               />
             </div>
@@ -114,7 +102,28 @@ const SimpleCalculator = () => {
                 className='px-4 py-2 border rounded-md text-black'
               />
             </div>
+            <div className='flex flex-col'>
+              <label className='mb-2'>input C: (H)</label>
+              <input
+                type='number'
+                value={highValue}
+                onChange={(e) => handleInputChange(e, setHighValue)}
+                className='px-4 py-2 border rounded-md text-black'
+              />
+            </div>
+            <div className='flex flex-col'>
+              <label className='mb-2'>input D: (L)</label>
+              <input
+                type='number'
+                value={lowValue}
+                onChange={(e) => handleInputChange(e, setLowValue)}
+                className='px-4 py-2 border rounded-md text-black'
+              />
+            </div>
           </div>
+          <p>
+            midpoint ={(parseFloat(highValue) + parseFloat(lowValue)) / 2}
+          </p>
           <button onClick={calculateResult} className='mt-4 px-6 py-2 border rounded-md bg-blue-500 text-white'>
             =
           </button>
@@ -157,27 +166,6 @@ const SimpleCalculator = () => {
                     </tbody>
                   </table>
                 </div>
-                {/**<div className='mt-6'>
-                  equator
-                  <table className='w-full'>
-                    <thead>
-                      <tr>
-                        <th className='p-1'>ID</th>
-                        <th className='p-1 text-green-600'>Positive</th>
-                        <th className='p-1 text-red-600'>Negative</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {midpointValues.map((value, index) => (
-                        <tr key={index}>
-                          <td className='p-1'>{index}</td>
-                          <td className='p-1'>{value.positive}</td>
-                          <td className='p-1'>{value.negative}</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                      </div>**/}
               </div>
             )}
           </div>
